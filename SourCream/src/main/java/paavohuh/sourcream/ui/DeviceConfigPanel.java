e87ab2f671e1958bc0c33c65249dfbdab2f83a6a
Paavo Huhtala <paavo.huhtala@gmail.com>
2016-03-04 20:53:08 +0200

    Add clock speed configuration

--- SourCream/src/main/java/paavohuh/sourcream/ui/DeviceConfigPanel.java
+++ SourCream/src/main/java/paavohuh/sourcream/ui/DeviceConfigPanel.java
@@ -1,10 +1,14 @@
 
 package paavohuh.sourcream.ui;
 
+import java.awt.BorderLayout;
 import java.awt.Dialog;
 import java.awt.GridLayout;
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
 import paavohuh.sourcream.configuration.DeviceConfiguration;
 
 /**
@@ -12,15 +16,36 @@ import paavohuh.sourcream.configuration.DeviceConfiguration;
  */
 class DeviceConfigPanel extends JPanel {
     private final DeviceConfiguration config;
-
     /**
      * Creates a new device configuration panel.
      * @param parent The parent.
      * @param config The device configuration.
      */
     public DeviceConfigPanel(Dialog parent, DeviceConfiguration config) {
-        this.setLayout(new GridLayout(5, 2));
-        add(new JLabel("These settings can and will affect emulation."));
+        setLayout(new BorderLayout(15, 15));
+        
+        JPanel clockSpeedGroup = new JPanel(new GridLayout(1, 2, 15, 15));
+        
+        clockSpeedGroup.setBorder(BorderFactory.createEmptyBorder(8, 8, 8, 8));
+        
+        SpinnerNumberModel clockSpeedSpinnerModel =
+            new SpinnerNumberModel(
+                config.getClockSpeed(),
+                100,
+                1000,
+                100);
+        
+        clockSpeedGroup.add(new JLabel("Emulated clock speed"));
+        
+        JSpinner clockSpeedSpinner = new JSpinner(clockSpeedSpinnerModel);
+        clockSpeedSpinner.addChangeListener(event -> {
+            int newClockSpeed = clockSpeedSpinnerModel.getNumber().intValue();
+            config.setClockSpeed(newClockSpeed);
+        });
+        clockSpeedGroup.add(clockSpeedSpinner);
+        
+        add(clockSpeedGroup, BorderLayout.NORTH);
+        
         this.config = config;
     }
 
